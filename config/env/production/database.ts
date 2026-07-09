3b85e19d80f5ace5b373be394f07799a13d4883c
kushagrasavya <kushagrasavya.choudhary@gmail.com>
2026-07-09 10:38:15 +0530

    Added production postgres config

---
+++ config/env/production/database.ts
@@ -0,0 +1,11 @@
+export default ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      connectionString: env('DATABASE_URL'),
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+  },
+});
